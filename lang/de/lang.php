c5591d2972034df13f95d39b7c8ec69077d216da
Andreas Gohr <andi@splitbrain.org>
2009-02-22 13:57:38 +0100

    German translation
    Ignore-this: dc101833877a78a971197e1f051ada78
    
    darcs-hash:20090222125738-7ad00-f715b8d97678bba38524684238b1d60e1d9df5f6.gz

---
+++ lang/de/lang.php
@@ -0,0 +1,51 @@
+<?php
+
+$lang['menu'] = 'Wikis abgleichen';
+
+$lang['syncstart'] = 'Abgleich starten';
+$lang['lastsync']  = 'Letzer Abgleich:';
+$lang['remotever'] = 'Entfernte Wiki Version:';
+$lang['noconnect'] = 'Konnte die Version des entfernten Wikis nicht abfragen. Stellen Sie sicher, dass das Wiki XMLRPX-Abfragen zulässt und das die URL korrekt konfiguriert wurde.';
+
+$lang['neversync'] = 'Dieses Profil wurde noch nie verwendet.';
+$lang['profile'] = 'Abgleich-Profil';
+$lang['newprofile'] = 'Neues Profil...';
+$lang['select'] = 'Auswählen';
+$lang['create'] = 'Neues Abgleich-Profil erstellen';
+$lang['edit'] = 'Abgleich-Profil bearbeiten';
+
+$lang['server'] = 'XMLRPC URL';
+$lang['user']   = 'Nutzername';
+$lang['pass']   = 'Passwort';
+$lang['ns']     = 'Namensraum';
+$lang['depth']  = 'Abgleich-Tiefe';
+$lang['level0'] = 'Alle Unternamensräume';
+$lang['level1'] = 'Keine Unternamensräume';
+$lang['level2'] = 'Namensraum + 1 Unternamensraum';
+$lang['level3'] = 'Namensraum + 2 Unternamensräume';
+
+$lang['save'] = 'Speichern';
+$lang['changewarn'] = 'Erneutes Speichern setzt die Abgleichzeiten für diese Profil zurück. Sie müssen dann die Abgleichrichtung für alle Dateien manuell festlegen.';
+
+$lang['lockfail']      = 'Konnte nicht gelockt werden und wird übersprungen:';
+$lang['pullfail']      = 'Runterladen fehlgeschalgen:';
+$lang['pullok']        = 'Runterladen erfolgreich:';
+$lang['localdel']      = 'Lokal gelöscht:';
+$lang['pushfail']      = 'Hochladen fehlgeschlagen:';
+$lang['pushok']        = 'Hochladen erfolgreich:';
+$lang['remotedelok']   = 'Entferntes Löschen erfolgreich:';
+$lang['remotedelfail'] = 'Entferntes Löschen fehlgeschlagen:';
+$lang['skipped']       = 'Übersprungen:';
+
+$lang['page']   = 'Seitenname';
+$lang['local']  = 'Lokales Wiki';
+$lang['remote'] = 'Entferntes Wiki';
+$lang['diff']   = 'Unterschied';
+
+$lang['push']    = 'Lade lokale version in entferntes Wiki hoch.';
+$lang['pushdel'] = 'Lösche die Version im entfernten Wiki.';
+$lang['pull']    = 'Lade die Version aus dem entfernten Wiki in das lokale Wiki.';
+$lang['pulldel'] = 'Lösche die lokale Version.';
+$lang['keep']    = 'Überspringe diese Datei und behalte beide Versionen bei.';
+
+$lang['syncdone'] = 'Abgleich abgeschlossen.';
